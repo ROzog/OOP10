4d43e5b20d7645dcadecd4f2d82cb8201d9eb387
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-10-30 09:34:18 +0100

    asd

--- src/task/lottery/first/Starter.java
+++ src/task/lottery/first/Starter.java
@@ -3,9 +3,8 @@ package task.lottery.first;
 public class Starter {
     public static void main(String[] args) {
         String[] names = {"Daniel", "Dawid", "Ilona", "Kacper"};
+
         Lottery lottery = new Lottery(names, "Grasz o 100000000 zł");
-
-
         System.out.println(lottery.getRandomWinner());
     }
 }
